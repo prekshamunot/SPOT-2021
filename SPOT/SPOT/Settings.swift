c9693633797d37f07720edafac53e8cbdb7c3b52
tilak02 <tilak.agarwal007@gmail.com>
2021-03-29 13:22:14 -0400

    settings

--- SPOT/SPOT/Settings.swift
+++ SPOT/SPOT/Settings.swift
@@ -12,22 +12,40 @@ struct Settings: View {
         
         NavigationView {
             VStack{
+                Spacer()
                 HStack{
+                    Spacer()
                     Text("My Settings").font(.custom("Cabin Bold", size: 30))
+                    Spacer()
                     Image("Mugar")
                         .resizable()
                         .frame(width: 78, height: 77)
                         .clipShape(RoundedRectangle(cornerRadius: 218))
                     .frame(width: 78, height: 77)
                     .shadow(color: Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.25)), radius:4, x:0, y:4)
+                    Spacer()
                 }
                 Spacer()
                 NavigationLink(destination: LoginView()) {
-                                Text("Logout").fontWeight(.semibold)
-                                    .font(.title)
+                                Text("Logout")
                                     .padding()
-                                    .foregroundColor(.white)
-                                    .background(Color.red)
+                                    .foregroundColor(.red)
+                                    .background(Color.white)
+                                    .cornerRadius(5)
+                                    
+                }.frame(width: 150, height: 60)
+                NavigationLink(destination: LoginView()) {
+                                Text("Send Feedback")
+                                    .padding()
+                                    .foregroundColor(.red)
+                                    .background(Color.white)
+                                    .cornerRadius(5)
+                }.frame(width: 250, height: 60)
+                NavigationLink(destination: LoginView()) {
+                                Text("About")
+                                    .padding()
+                                    .foregroundColor(.red)
+                                    .background(Color.white)
                                     .cornerRadius(5)
                 }.frame(width: 150, height: 60)
                 Spacer()
